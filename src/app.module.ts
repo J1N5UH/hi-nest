6ad20725c3537243d56929f4a5bef7a9ad13ca21
J1N5UH <jinsuh8375@gmail.com>
2023-09-20 14:12:22 +0900

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './movies/app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController], //클라이언트의 요청을 처리
-  providers: [MoviesService], // 비즈니스 로직 구현
+  imports: [MoviesModule],
+  controllers: [AppController], //클라이언트의 요청을 처리
+  providers: [], // 비즈니스 로직 구현
 })
 export class AppModule {}
